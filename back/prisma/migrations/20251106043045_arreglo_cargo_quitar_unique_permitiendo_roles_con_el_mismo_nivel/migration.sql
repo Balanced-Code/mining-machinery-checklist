cd8fb64f74891fe0b11db9e0fc0bfad9bdef792a
Kleis <leosmv98@gmail.com>
2025-12-02 06:11:58 -0300

    chore: ocultar migraciones de Prisma en gitignore

--- back/prisma/migrations/20251106043045_arreglo_cargo_quitar_unique_permitiendo_roles_con_el_mismo_nivel/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "public"."cargo_nivel_key";
-
--- CreateIndex
-CREATE INDEX "cargo_nivel_idx" ON "cargo"("nivel");
--- back/prisma/migrations/20251108004501_cambiar_fechas_inspeccion_a_timestamp/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "inspeccion" ALTER COLUMN "fecha_inicio" SET DATA TYPE TIMESTAMP,
-ALTER COLUMN "fecha_finalizacion" SET DATA TYPE TIMESTAMP;
